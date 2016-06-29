73efa13d3ab88f0c1c66fc881cc8592c1bf1abb5
amberdoig <amberdoig@gmail.com>
2016-06-28 20:31:27 -0700

    first attempt at plotting function

---
+++ plottools.py
@@ -0,0 +1,24 @@
+from twentyonecmfast_tools import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plotter(glob_one,glob_two):
+    """User defines two fileglobs
+    recommended that variables be set first 
+    or whole address can be entered"""
+
+p,k,d,e=load_andre_models(glob_one)
+P,K,D,D=load_andre_models(glob_two)
+
+onez=np.argsort(p[:,0])
+twoz=np.argsort(P[:,0])
+
+plt.plot(p[onez,0],p[onez,5], color='r', label='glob1')
+plt.plot(P[twoz,0],P[twoz,5], color='b', label='glob2')
+
+plt.ylabel('avg temp')
+plt.xlabel('z')
+plt.title('z vs. avg temp')
+plt.legend(loc='lower right')
+
+plt.show()
