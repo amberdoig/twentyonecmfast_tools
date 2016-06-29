d7e31380d12086f3e761db032a22c1b75665d6f1
amberdoig <amberdoig@gmail.com>
2016-06-29 15:56:07 -0700

    now accepts user input for file globs

--- plottools.py
+++ plottools.py
@@ -7,18 +7,18 @@ def plotter(glob_one,glob_two):
     recommended that variables be set first 
     or whole address can be entered"""
 
-p,k,d,e=load_andre_models(glob_one)
-P,K,D,E=load_andre_models(glob_two)
+    p,k,d,e=load_andre_models(glob_one)
+    P,K,D,E=load_andre_models(glob_two)
 
-onez=np.argsort(p[:,0])
-twoz=np.argsort(P[:,0])
+    onez=np.argsort(p[:,0])
+    twoz=np.argsort(P[:,0])
 
-plt.plot(p[onez,0],p[onez,5], color='r', label='glob1')
-plt.plot(P[twoz,0],P[twoz,5], color='b', label='glob2')
+    plt.plot(p[onez,0],p[onez,5], color='r', label='glob1')
+    plt.plot(P[twoz,0],P[twoz,5], color='b', label='glob2')
 
-plt.ylabel('avg temp')
-plt.xlabel('z')
-plt.title('z vs. avg temp')
-plt.legend(loc='lower right')
+    plt.ylabel('avg temp')
+    plt.xlabel('z')
+    plt.title('z vs. avg temp')
+    plt.legend(loc='lower right')
 
-plt.show()
+    plt.show()
