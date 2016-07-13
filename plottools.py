6313a42ebd6510bd23d43111d18667269ade7053
amberdoig <amberdoig@gmail.com>
2016-07-13 15:42:11 -0700

    user specifies either or both axes, labels and title update accordingly

--- plottools.py
+++ plottools.py
@@ -2,23 +2,37 @@ from twentyonecmfast_tools import *
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plotter(glob_one,glob_two):
+def plotter(glob_one,glob_two,y_axis,x_axis=0):
     """User defines two fileglobs
     recommended that variables be set first 
-    or whole address can be entered"""
+    or whole address can be entered
+    0: z
+    1: Nf
+    2: Nx
+    3: alphaX
+    4: Mmin
+    5: avg temp
+    """
+
+    labels_dict = {0 : 'z',
+                   1 : 'Nf',
+                   2 : 'Nx',
+                   3 : 'alphaX',
+                   4 : 'Mmin',
+                   5 : 'avg temp'}
 
     p,k,d,e=load_andre_models(glob_one)
     P,K,D,E=load_andre_models(glob_two)
 
-    onez=np.argsort(p[:,0])
-    twoz=np.argsort(P[:,0])
+    onez=np.argsort(p[:,x_axis])
+    twoz=np.argsort(P[:,x_axis])
 
-    plt.plot(p[onez,0],p[onez,5], color='r', label='glob1')
-    plt.plot(P[twoz,0],P[twoz,5], color='b', label='glob2')
+    plt.plot(p[onez,x_axis],p[onez,y_axis], color='r', label='glob1')
+    plt.plot(P[twoz,x_axis],P[twoz,y_axis], color='b', label='glob2')
 
-    plt.ylabel('avg temp')
-    plt.xlabel('z')
-    plt.title('z vs. avg temp')
+    plt.ylabel(labels_dict[y_axis])
+    plt.xlabel(labels_dict[x_axis])
+    plt.title(labels_dict[y_axis]+' as a function of '+labels_dict[x_axis])
     plt.legend(loc='lower right')
 
     plt.show()
