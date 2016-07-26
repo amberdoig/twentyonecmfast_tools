924f291f80fc3e297d78f8ae43657819ced2b989
amberdoig <amberdoig@gmail.com>
2016-07-25 20:52:36 -0700

    added slicedata and shape is pulled from file name

--- 3dplot.py
+++ 3dplot.py
@@ -9,4 +9,13 @@ def sliceplot(file_glob,xslice):
     data=data.reshape((400,400,400))
     plt.imshow(data[xslice,:,:])
     plt.colorbar()
-    plt.show()    
+    plt.show() 
+
+def slicedata(file_glob,xslice):
+    """User inputs location of binary data file
+    and single slice of x axis is retured as data"""
+
+    data=np.fromfile(file_glob,dtype=np.float32)
+    lst=file_glob.split('_')
+    data=data.reshape((lst[13],lst[13],lst[13]))
+    print(data)   
